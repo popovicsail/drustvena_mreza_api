77d475f0619afd4bc735c93a4b980e5a918771a2
aleksandar popovic <popovicsail@gmail.com>
2025-05-29 23:26:28 +0200

    feat: dodate funkcionalnosti za databaze za grupe

--- drustvena_mreza/Controllers/GrupaController.cs
+++ drustvena_mreza/Controllers/GrupaController.cs
@@ -11,15 +11,24 @@ namespace drustvena_mreza.Controllers
     [ApiController]
     public class GrupaController : ControllerBase
     {
+        GrupaDBRepository GrupaDB { get; set; } = new GrupaDBRepository();
 
         [HttpGet]
-        public ActionResult<List<Grupa>> GetAll()   //U Grupa.cs sam napravio klasu (GrupaDTO) da mogu konvertovati vrijeme u string i prikazati u dobrom formatu
+        public ActionResult<List<Grupa>> GetAll()
         {
-            List<Grupa> grupeList = GetAllGrupaFromDatabase();
+            List<Grupa> grupeList = GrupaDB.GetAll();
             return grupeList;
         }
 
-        
+
+        [HttpGet("{inputId}")]
+        public ActionResult<Korisnik> GetById(int inputId)
+        {
+            Grupa grupa = GrupaDB.GetById(inputId);
+
+            return Ok(grupa);
+        }
+
 
         [HttpPost]
         public ActionResult<Grupa> Create([FromBody] Grupa newGrupa)
@@ -28,74 +37,19 @@ namespace drustvena_mreza.Controllers
             {
                 return BadRequest();
             }
-            if (GrupaRepository.AllGrupa.ContainsKey(newGrupa.Id))
-            {
-                return BadRequest();
-            }
-            GrupaRepository.AllGrupa[newGrupa.Id] = newGrupa;
-            GrupaRepository.SaveGrupa();
+
+            Grupa grupa = GrupaDB.Create(newGrupa);
+
             return Ok(newGrupa);
         }
+
 
         [HttpDelete("{id}")]
         public ActionResult Delete(int id)
         {
-            if (!GrupaRepository.AllGrupa.ContainsKey(id))
-            {
-                return NotFound();
-            }
+            GrupaDB.Delete(id);
 
-            GrupaRepository.AllGrupa.Remove(id);
-            GrupaRepository.SaveGrupa();
             return NoContent();
         }
-
-        private List<Grupa> GetAllGrupaFromDatabase()
-        {
-            try
-            {
-                using SqliteConnection sqliteConnection = new SqliteConnection("Data Source=data/database.db");
-                sqliteConnection.Open();
-
-                string sqliteQuery = "SELECT * FROM Groups";
-                using SqliteCommand sqliteCommand = new SqliteCommand(sqliteQuery, sqliteConnection);
-
-                using SqliteDataReader sqliteDataReader = sqliteCommand.ExecuteReader();
-
-                List<Grupa> AllGrupa = new List<Grupa>();
-
-                while (sqliteDataReader.Read())
-                {
-                    int grupaId = sqliteDataReader.GetInt32(0);
-                    string grupaIme = sqliteDataReader.GetString(1);
-                    string grupaDatumOsnivanja = sqliteDataReader.GetString(2);
-
-                    Grupa newGrupa = new Grupa(grupaId, grupaIme, grupaDatumOsnivanja);
-
-                    AllGrupa.Add(newGrupa);
-                }
-
-                return AllGrupa;
-            }
-            catch (SqliteException ex)
-            {
-                Console.WriteLine($"Greška pri konekciji ili izvršavanju neispravnih SQL upita: {ex.Message}");
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
-            }
-            catch (InvalidOperationException ex)
-            {
-                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Neočekivana greška: {ex.Message}");
-            }
-
-            return null;
-        }
-
     }
 }
